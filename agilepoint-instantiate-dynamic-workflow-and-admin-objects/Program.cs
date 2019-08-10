01c05714cf2b413735b8d3ce69eeb80f7423c433
Vinayaka Amaresh <dvinun@gmail.com>
2019-08-09 18:15:31 -0700

    Code sample to create WCF/REST workflow and admin objects in AgilePoint

---
+++ agilepoint-instantiate-dynamic-workflow-and-admin-objects/Program.cs
@@ -0,0 +1,104 @@
+﻿using Ascentn.AgilePoint.WCFClient;
+using Ascentn.Workflow.Base;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.ServiceModel;
+
+
+namespace HttpClientDemo
+{
+    class Program
+    {
+        static string userNameWithDomain;
+        static string passWord;
+        static string AuthHeader;
+        static void Main(string[] args)
+        {
+             userNameWithDomain = "ap-fvzlpv2\\agpsvc";
+             passWord = "Pass@word88";
+             AuthHeader = string.Format("Basic " + Base64Encode(string.Format("{0}:{1}", userNameWithDomain, passWord)));
+
+            try
+            {
+                SetCustomAttribute();
+                WFManualWorkItem workItemResponse =GetWorkItem("804738002502C7551199AE51BDBC1BEF");
+                IWFWorkflowService wfService = GetWorkflowService();
+                WFManualWorkItem wFProcessDefinitions = wfService.GetWorkItem("804738002502C7551199AE51BDBC1BEF");               
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\nException Caught!");
+               
+            }
+        }
+
+    
+
+        public static IWFWorkflowService GetWorkflowService()
+        {
+            Uri uri = new Uri("net.tcp://AP-FVZLPV2:13488/AgilePointServer/Workflow");
+            IWFWorkflowService m_api;
+            const string APPLICATION_NAME = "SPSIntegration";
+            string SVCuserName = "agpsvc";
+            string SVCPassWord = "Pass@word88";
+            string SVCUserDomain = "AP-FVZLPV2";
+            string svcAdminUserNameWithDomain = string.Format("{0}\\{1}", SVCUserDomain, SVCuserName);
+            string imporsantorUser = "AP-FVZLPV2\\agpuser1";
+            System.Net.ICredentials credentials = new NetworkCredential(SVCuserName, SVCPassWord, SVCUserDomain);
+            var myBinding = new NetTcpBinding();
+            string nettcpurl = "net.tcp://AP-FVZLPV2:13488/AgilePointServer/Workflow";
+            string locale = "en-Us";
+            m_api = new WCFWorkflowProxy(APPLICATION_NAME, "AP-FVZLPV2", locale, imporsantorUser, credentials, myBinding, nettcpurl, svcAdminUserNameWithDomain);
+            string authenticatedUserName = m_api.CheckAuthenticated();
+            return m_api;
+        }
+
+        public static WFManualWorkItem  GetWorkItem(string workItemId)
+        {
+            string url = string.Format("http://ap-fvzlpv2:13490/agilepointserver/workflow/GetWorkItem/{0}", workItemId);
+            WebHeaderCollection headerCollection = new WebHeaderCollection();
+            headerCollection.Add("Authorization", AuthHeader);
+            string Response = RestUtility.MakeRESTHTTPRequest(url, System.Net.Http.HttpMethod.Get, headerCollection, string.Empty, "application/json");
+            ManualWorkItemResponse manualWorkItemResponse= JsonConvert.DeserializeObject<ManualWorkItemResponse>(Response);
+            return  manualWorkItemResponse.GetWorkItemResult;
+        }
+
+        public static void SetCustomAttribute()
+        {
+            string url = string.Format("http://ap-fvzlpv2:13490/agilepointserver/workflow/SetCustomAttrs/{0}", "4C04E35AD1914CD69A1A68D5AB52DB4C");
+            WebHeaderCollection headerCollection = new WebHeaderCollection();
+            headerCollection.Add("Authorization", AuthHeader);
+            List<NameValue> list = new List<NameValue>();
+            list.Add(new NameValue("/pd:AP/pd:processFields/pd:text1", "Value1"));
+            CustAttributeRequest custAttributeRequest = new CustAttributeRequest();
+            custAttributeRequest.attributes = list;
+            string payload = JsonConvert.SerializeObject(custAttributeRequest);
+            string Response = RestUtility.MakeRESTHTTPRequest(url, System.Net.Http.HttpMethod.Post, headerCollection, payload, "application/json");
+           // ManualWorkItemResponse manualWorkItemResponse = JsonConvert.DeserializeObject<ManualWorkItemResponse>(Response);
+
+
+        }
+
+        public static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+    }
+
+    public class ManualWorkItemResponse
+    {
+
+        public WFManualWorkItem GetWorkItemResult { get; set; }
+    }
+    
+
+    public class CustAttributeRequest
+    {
+        public List<NameValue> attributes { get; set; }
+    }
+}
---
+++ agilepoint-instantiate-dynamic-workflow-and-admin-objects/RestUtility.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientDemo
+{
+    static class RestUtility
+    {
+
+      
+
+        public static string MakeRESTHTTPRequest(string RequestURL, HttpMethod RequestMethod, WebHeaderCollection webHeaderCollection, string RequestData,string contentType)
+        {
+            string content = string.Empty;
+            try
+            {
+                 var HttpRequest = (HttpWebRequest)WebRequest.Create(RequestURL);
+
+                HttpRequest.Accept = "application/json";                
+                HttpRequest.Method = RequestMethod.ToString();
+                HttpRequest.Headers = webHeaderCollection;
+                if (!string.IsNullOrEmpty(contentType))
+                {
+                    HttpRequest.ContentType = contentType;
+                }
+                else
+                    HttpRequest.ContentType = "application/json";
+                if (RequestMethod.Method.ToString() == HttpMethod.Post.ToString())
+                {
+                    byte[] byteArray = Encoding.UTF8.GetBytes(RequestData);
+                    HttpRequest.ContentLength = byteArray.Length;
+                    using (Stream dataStream = HttpRequest.GetRequestStream())
+                    {
+                        // Write the data to the request stream.
+                        dataStream.Write(byteArray, 0, byteArray.Length);
+                        // Close the Stream object.
+                        dataStream.Close();
+                    }
+                }
+                using (var response = (HttpWebResponse)HttpRequest.GetResponse())
+                {
+                    using (var stream = response.GetResponseStream())
+                    {
+                        using (var sr = new StreamReader(stream))
+                        {
+                            content = sr.ReadToEnd();
+                        }
+                    }
+                }
+                return content;
+            }
+            catch (WebException ex)
+            {
+                string WebExceptionStream = string.Empty;
+                if (ex.Response != null)
+                {
+                    using (StreamReader responseReader = new StreamReader(ex.Response.GetResponseStream()))
+                    {
+                        WebExceptionStream = responseReader.ReadToEnd();
+                        throw new Exception(WebExceptionStream);
+                    }
+                }
+                else
+                    throw;
+            }
+            catch (Exception Ex)
+            {
+                throw;
+            }
+            return content;
+
+        }
+
+
+    }
+}
